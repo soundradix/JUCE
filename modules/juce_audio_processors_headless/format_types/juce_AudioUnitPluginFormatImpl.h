soundradix/JUCE

commit 4b8454a20c4ddd81ffb4602d94231cf002618d48

AU Host: Unify macOS and iOS parameter listener behaviour
Calling AudioUnitGetParameter too frequently seems to cause problems in
multibus plugins on iOS. The new implementation caches the parameter
value on the hosting side and only updates it when a callback is
received from the plugin.

modules/juce_audio_processors_headless/format_types/juce_AudioUnitPluginFormatImpl.h
@@ -36,10 +36,11 @@
 
 #if JUCE_INTERNAL_HAS_AU
 
+#include <AudioToolbox/AudioUnitUtilities.h>
+
 #if JUCE_MAC
 #include <AudioUnit/AUCocoaUIView.h>
 #include <CoreAudioKit/AUGenericView.h>
-#include <AudioToolbox/AudioUnitUtilities.h>
 
 #if JUCE_INTERNAL_HAS_ARA
  #include <ARA_API/ARAAudioUnit.h>
@@ -613,30 +614,33 @@ public:
 
         float getValue() const override
         {
-            const ScopedLock sl (pluginInstance.lock);
+            return cachedValue;
+        }
 
-            AudioUnitParameterValue value = 0;
-
-            if (auto* au = pluginInstance.audioUnit)
-            {
-                AudioUnitGetParameter (au, paramID, kAudioUnitScope_Global, 0, &value);
-                value = normaliseParamValue (value);
-            }
-
-            return value;
+        void updateCachedValueAndNotify (float newValue)
+        {
+            cachedValue = newValue;
+            sendValueChangedMessageToListeners (cachedValue);
         }
 
         void setValue (float newValue) override
         {
             const ScopedLock sl (pluginInstance.lock);
 
-            if (auto* au = pluginInstance.audioUnit)
-            {
-                AudioUnitSetParameter (au, paramID, kAudioUnitScope_Global,
-                                       0, scaleParamValue (newValue), 0);
+            cachedValue = newValue;
 
-                sendParameterChangeEvent();
-            }
+            auto* au = pluginInstance.audioUnit;
+
+            if (au == nullptr)
+                return;
+
+            AudioUnitParameter parameter;
+            parameter.mParameterID = paramID;
+            parameter.mAudioUnit = au;
+            parameter.mScope = kAudioUnitScope_Global;
+            parameter.mElement = 0;
+
+            AUParameterSet (pluginInstance.eventListenerRef, nullptr, &parameter, scaleParamValue (newValue), 0);
         }
 
         float getDefaultValue() const override
@@ -802,6 +806,7 @@ public:
         const bool valuesHaveStrings, isSwitch;
         String valueLabel;
         const AudioUnitParameterValue defaultValue;
+        AudioUnitParameterValue cachedValue = defaultValue;
         StringArray auValueStrings;
     };
 
@@ -809,9 +814,7 @@ public:
         : AudioPluginInstance (getBusesProperties (au)),
           auComponent (AudioComponentInstanceGetComponent (au)),
           audioUnit (au),
-        #if JUCE_MAC
           eventListenerRef (nullptr),
-        #endif
           midiConcatenator (2048)
     {
         using namespace AudioUnitFormatHelpers;
@@ -874,9 +877,7 @@ public:
     // called from the destructor above
     void cleanup()
     {
-       #if JUCE_MAC
         disposeEventListener();
-       #endif
 
         if (prepared)
             releaseResources();
@@ -892,10 +893,7 @@ public:
         setLatencySamples (0);
         refreshParameterList();
         setPluginCallbacks();
-
-       #if JUCE_MAC
         createEventListener();
-       #endif
 
         return true;
     }
@@ -1488,7 +1486,6 @@ public:
     //==============================================================================
     void sendAllParametersChangedEvents()
     {
-       #if JUCE_MAC
         jassert (audioUnit != nullptr);
 
         AudioUnitParameter param;
@@ -1496,7 +1493,6 @@ public:
         param.mParameterID = kAUParameterListener_AnyParameter;
 
         AUParameterListenerNotify (nullptr, nullptr, &param);
-       #endif
     }
 
     //==============================================================================
@@ -1874,7 +1870,6 @@ private:
                     AudioUnitSetProperty (parent.audioUnit, kAudioUnitProperty_BypassEffect,
                                           kAudioUnitScope_Global, 0, &value, sizeof (UInt32));
 
-                   #if JUCE_MAC
                     jassert (parent.audioUnit != nullptr);
 
                     AudioUnitEvent ev;
@@ -1885,7 +1880,6 @@ private:
                     ev.mArgument.mProperty.mElement     = 0;
 
                     AUEventListenerNotify (parent.eventListenerRef, nullptr, &ev);
-                   #endif
                 }
             }
         }
@@ -1934,9 +1928,7 @@ private:
     HeapBlock<AUChannelInfo> channelInfos;
 
     AudioUnit audioUnit;
-   #if JUCE_MAC
     AUEventListenerRef eventListenerRef;
-   #endif
 
     std::map<UInt32, AUInstanceParameter*> paramIDToParameter;
 
@@ -1979,7 +1971,6 @@ private:
         }
     }
 
-   #if JUCE_MAC
     void disposeEventListener()
     {
         if (eventListenerRef != nullptr)
@@ -2052,7 +2043,7 @@ private:
             return;
 
         if (event.mEventType == kAudioUnitEvent_ParameterValueChange)
-            param->sendValueChangedMessageToListeners (param->normaliseParamValue (newValue));
+            param->updateCachedValueAndNotify (param->normaliseParamValue (newValue));
         else if (event.mEventType == kAudioUnitEvent_BeginParameterChangeGesture)
             param->beginChangeGesture();
         else if (event.mEventType == kAudioUnitEvent_EndParameterChangeGesture)
@@ -2109,7 +2100,6 @@ private:
             param->setLabel (getParamLabel (info.get()));
         }
     }
-   #endif
 
     /*  Some fields in the AudioUnitParameterInfo may need to be released after use,
         so we'll do that using RAII.

commit 4b5be5d25aaa07f9b13492847128a8a2482e8851

AU Host: After setting a new plugin state, immediately update all cached parameter values
This addresses a regression introduced in
4b8454a20c4ddd81ffb4602d94231cf002618d48.

In pluginval, the "Plugin state restoration" test could sometimes fail
for AU plugins. The cached AU parameter value wasn't updated until a
parameter-change notification was received by the host. This
notification is sent asynchronously by the system, so fetching the
parameter value immediately after setting a new plugin state could
end up returning a stale parameter value.

modules/juce_audio_processors_headless/format_types/juce_AudioUnitPluginFormatImpl.h
@@ -617,10 +617,22 @@ public:
             return cachedValue;
         }
 
+        void syncCachedValue()
+        {
+            const ScopedLock sl (pluginInstance.lock);
+
+            AudioUnitParameterValue newValue{};
+
+            if (AudioUnitGetParameter (pluginInstance.audioUnit, paramID, kAudioUnitScope_Global, 0, &newValue) != noErr)
+                return;
+
+            cachedValue = normaliseParamValue (newValue);
+        }
+
         void updateCachedValueAndNotify (float newValue)
         {
             cachedValue = newValue;
-            sendValueChangedMessageToListeners (cachedValue);
+            sendValueChangedMessageToListeners (newValue);
         }
 
         void setValue (float newValue) override
@@ -1487,6 +1499,14 @@ public:
     void sendAllParametersChangedEvents()
     {
         jassert (audioUnit != nullptr);
+
+        for (const auto& idAndParam : paramIDToParameter)
+        {
+            if (auto* param = idAndParam.second)
+            {
+                param->syncCachedValue();
+            }
+        }
 
         AudioUnitParameter param;
         param.mAudioUnit = audioUnit;