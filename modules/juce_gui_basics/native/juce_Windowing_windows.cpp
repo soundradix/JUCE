6770b97ed57e7b4162200ad922b89b83bda1db95
attila <attila@juce.com>
2026-05-25 16:54:13 +0200

    Windows: Repaint the peer after calling setCurrentRenderingEngine
    This change corrects an unintended change introduced in
    258203706cdfc1357effe8cc752672b793cea37d.

--- modules/juce_gui_basics/native/juce_Windowing_windows.cpp
+++ modules/juce_gui_basics/native/juce_Windowing_windows.cpp
@@ -5490,6 +5490,7 @@ void HWNDComponentPeer::setCurrentRenderingEngine (int e)
         // can only be created once per window.
         renderContext.reset();
         renderContext = contextDescriptors[e].construct (*this);
+        InvalidateRect (hwnd, nullptr, TRUE);
     }
 }
 
