f7bee840f93e40e0578e16fcf94f6c5c89bf823a
Yair Chuchem <yairchu@gmail.com>
2026-07-13 17:11:36 +0300

    Remove redundant member initialization (init twice)

--- modules/juce_opengl/opengl/juce_OpenGLTexture.cpp
+++ modules/juce_opengl/opengl/juce_OpenGLTexture.cpp
@@ -73,7 +73,6 @@ void OpenGLTexture::create (const int w, const int h, const void* pixels, GLenum
 
     width  = getAllowedTextureSize (w);
     height = getAllowedTextureSize (h);
-    flipped = false;
 
     const GLint internalformat = type == GL_ALPHA ? GL_ALPHA : GL_RGBA;
 
